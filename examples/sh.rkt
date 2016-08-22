36c4c50bc64e73f5cc95a4f59cd8aa4b6c01328b
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-08-21 21:01:24 -0400

    Toy Racket shell

---
+++ examples/sh.rkt
@@ -0,0 +1,55 @@
+#lang something/shell
+
+require
+  something/for
+  tabular
+
+///////////////////////////////////////////////////////////////////////////
+// Sketches of utilities
+
+def space-separated-columns (converters [])
+  local-require racket/string
+  def header: map (string-split (read-line)) string->symbol
+  def nsplits: length header - 1
+  def split-once s:
+    match s
+      pregexp "^\\s*(\\S+)\\s+(\\S.*)?$" [_, h, t]: values h t
+      _: values s #f
+  def split-line line n converters:
+    if zero? n
+       match converters
+         []: list line
+         cons c _: list ((c || values) line)
+       let
+         def-values h t: split-once line
+         match converters
+           []:       cons                h  (split-line t (n - 1) [])
+           cons c r: cons ((c || values) h) (split-line t (n - 1) r )
+  cons header (for/list { line: (in-lines) }: split-line line nsplits converters)
+
+def ((preserve-header nlines thunk))
+  for { n : in-range nlines }
+    displayln (read-line)
+  (thunk)
+
+def (discard)
+  for { line : (in-lines) }
+    (void)
+
+def (ignore)
+  copy-port (current-input-port) (current-output-port)
+
+///////////////////////////////////////////////////////////////////////////
+// Simple demos
+
+ls -la | grep "^d" | fgrep -v "."
+
+pipeline
+  ps -wwwax
+  preserve-header 1 {: grep "racket" }
+  space-separated-columns [string->number]
+
+def message-box text:
+  whiptail --title "Testing" --ok-button "OK" --msgbox text 8 50
+
+message-box "This is pretty cool."
---
+++ src/something/shell.rkt
@@ -0,0 +1,144 @@
+#lang something
+
+require
+  rename-in something ('#%app' base-app) ('#%top' base-top)
+  only-in something/lang/reader read-toplevel-syntax
+  racket/system
+  racket/format
+  only-in racket/list flatten
+  racket/port
+
+  for-syntax something
+  for-syntax syntax/stx
+
+provide
+  except-out (all-from-out something) '#%module-begin'
+  rename-out ('#%plain-module-begin' '#%module-begin')
+  all-from-out racket/port
+  rename-out (shell-app '#%app') (shell-top '#%top')
+  run-in-background
+  pipeline
+  pipe
+  wait
+
+def-syntax shell-app stx
+  syntax-case stx []
+    _ f arg ...
+      identifier? (syntax f) && identifier-binding (syntax f)
+      (syntax (base-app f arg ...))
+    _ f arg ...
+      identifier? (syntax f)
+      build-command (syntax f) (syntax (arg ...))
+
+def-syntax shell-top stx
+  def id: stx-cdr stx
+  if identifier-binding id
+     datum->syntax stx (cons (syntax base-top) id)
+     build-command id (syntax ())
+
+begin-for-syntax
+  def build-command id-stx args-stx
+    def command: symbol->string (syntax-e id-stx)
+    with-syntax { (arg ...): args-stx }
+      quasisyntax (apply system*/exit-code
+                         (find-command (unsyntax command))
+                         (flatten (list (parse-shell-argument arg) ...)))
+
+def find-command command-name:
+  or (find-executable-path command-name) \
+     (error `find-command "No such command: ~v" command-name)
+
+def-syntax parse-shell-argument stx
+  syntax-case stx []
+    _ value
+      number? (syntax-e (syntax value))
+      datum->syntax stx (number->string (syntax-e (syntax value)))
+    _ id
+      identifier? (syntax id) && regexp-match "^-" (symbol->string (syntax-e (syntax id)))
+      datum->syntax stx (symbol->string (syntax-e (syntax id)))
+    _ expr
+      (syntax (format-shell-argument expr))
+
+def format-shell-argument a
+  cond
+    when string? a: a
+    when symbol? a: symbol->string a
+    when number? a: '~a' a
+    when list? a: map a format-shell-argument
+    else: error (quote format-shell-argument) "Cannot format ~v" a
+
+def-operator & 8 postfix run-in-background
+def-syntax run-in-background stx
+  syntax-case stx [block]
+    _ (block expr ...)
+      (syntax (run-in-background (begin expr ...)))
+    _ expr
+      (syntax (shell-thread :: expr))
+
+def shell-thread thunk
+  def ch: (make-channel)
+  thread ::
+    with-handlers
+      when values e:
+        channel-put ch e
+      channel-put ch (thunk)
+  ch
+
+def-syntax ensure stx
+  syntax-case stx [block]
+    _ finally-expr (block body ...)
+      syntax (with-handlers:
+                when values e:
+                  finally-expr
+                  raise e
+                (begin0 (begin body ...) finally-expr))
+
+def-syntax pipeline stx
+  syntax-case stx [block]
+    _ (block)
+      (syntax (copy-port (current-input-port) (current-output-port)))
+    _ (block final-stage)
+      (syntax final-stage)
+    _ (block stage0 stage ...)
+      (syntax (pipe stage0 (pipeline (block stage ...))))
+
+def-operator | 10 left pipe
+def-syntax pipe stx
+  syntax-case stx []
+    _ lhs rhs
+      (syntax (pipe* {: run-pipe-stage lhs} {: run-pipe-stage rhs}))
+
+def run-pipe-stage result:
+  if procedure? result
+     (result)
+     result
+
+def pipe* lhs-thunk rhs-thunk:
+  def-values i o: (make-pipe)
+  def lhs-thread:
+    parameterize [current-output-port o]
+      (ensure (close-output-port o):
+        begin0 (lhs-thunk) (flush-output o)) &
+  def rhs-thread:
+    parameterize [current-input-port i]
+      (ensure (close-input-port i):
+        (rhs-thunk)) &
+  wait lhs-thread
+  wait rhs-thread
+
+def wait ch:
+  match channel-get ch
+    ? exn? e: raise e
+    v: v
+
+current-read-interaction read-toplevel-syntax
+
+module+ reader
+  require
+    something/lang/reader
+  provide
+    rename-out (read-shell-syntax read-syntax)
+    read-toplevel-syntax
+
+  def read-shell-syntax src p
+    read-syntax src p :language (syntax something/shell)
